062b312d97a0b17bddbddbed9789e0f8953af406
Sandy Carter <bwrsandman@gmail.com>
2015-08-12 20:12:31 -0400

    Factor out duplicated metadata about package

--- openupgradelib/__init__.py
+++ openupgradelib/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
-__author__ = 'Odoo Community Association'
+__author__ = 'Odoo Community Association (OCA)'
 __email__ = 'support@odoo-community.org'
-__version__ = '0.1.0'
+__version__ = '1.0.0'
+__doc__ = """A library with support functions to be called from Odoo \
+migration scripts."""
+__license__ = "AGPL-3"
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import openupgradelib
 
 try:
     from setuptools import setup
@@ -32,18 +33,17 @@ test_requirements = [
 
 setup(
     name='openupgradelib',
-    version='1.0.0',
-    description="A library with support functions to be called from Odoo "
-                "migration scripts.",
+    version=openupgradelib.__version__,
+    description=openupgradelib.__doc__,
     long_description=readme + '\n\n' + history,
-    author="Odoo Community Association",
-    author_email='support@odoo-community.org',
+    author=openupgradelib.__author__,
+    author_email=openupgradelib.__email__,
     url='https://github.com/OCA/openupgradelib',
     packages=['openupgradelib'],
     package_dir={'openupgradelib': 'openupgradelib'},
     include_package_data=True,
     install_requires=requirements,
-    license="AGPL-3",
+    license=openupgradelib.__license__,
     zip_safe=False,
     keywords='openupgradelib',
     classifiers=[
