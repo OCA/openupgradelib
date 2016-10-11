0b7a27f350daf52b8f28097604fd81647614faa6
Holger Brunn <hbrunn@therp.nl>
2016-10-11 18:49:04 +0000

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
+import os
 from . import test_openupgradelib
-from . import test_openupgradelib_with_openupgrade
+# only run openupgrade tests with python 2.7 on travis
+if os.environ.get('TRAVIS_PYTHON_VERSION') == '2.7':
+    from . import test_openupgradelib_with_openupgrade
+else:
+    import unittest
+    class test_openupgradelib_with_openupgrade(unittest.TestCase):
+        pass
