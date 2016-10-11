b8566cd6e549527ce86c958246b5459330b6b23c
Holger Brunn <hbrunn@therp.nl>
2016-10-11 18:38:10 +0000

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,9 @@
 import os
 from . import test_openupgradelib
 # only run openupgrade tests with python 2.7 on travis
-if os.environ.get('TRAVIS_PYTHON_VERSION') == '2.7':
+# weirdly, the variable TRAVIS_PYTHON_VERSION also has the value of
+# 2.7 for pypy at this point, so we cannot simply use this variable
+if 'openupgradelib' in os.environ.get('PYTHONPATH', ''):
     from . import test_openupgradelib_with_openupgrade
 else:
     import unittest
