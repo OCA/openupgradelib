3792355fe285fd974405d3e32b9555fe1313d0c8
Moisés López <moylop260@vauxoo.com>
2017-10-03 14:17:51 +0200

    [REF] bumpversion 1.3.2 signing commit with '-s'

--- openupgradelib/__init__.py
+++ openupgradelib/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Odoo Community Association (OCA)'
 __email__ = 'support@odoo-community.org'
-__version__ = '1.3.0'
+__version__ = '1.3.2'
 __doc__ = """A library with support functions to be called from Odoo \
 migration scripts."""
 __license__ = "AGPL-3"
