fedf87f1d1506820109450797098038b13a68cb3
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-09-16 12:19:01 +0200

    3.0.0

--- openupgradelib/__init__.py
+++ openupgradelib/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Odoo Community Association (OCA)'
 __email__ = 'support@odoo-community.org'
-__version__ = '2.0.0'
+__version__ = '3.0.0'
 __doc__ = """A library with support functions to be called from Odoo \
 migration scripts."""
 __license__ = "AGPL-3"
