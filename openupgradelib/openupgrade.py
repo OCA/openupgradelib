OCA/openupgradelib

commit ffbeb462825136e9139e371f3db081213c7b59b8

[IMP] Faster workflow deletion
Following #184, here I'm adding more indexes to speed up workflow deletion even more. Let's save even more hours.

This time, I'm also adding `drop_indexes=False` argument. The created index names matches those created/deleted by Odoo addon update. This way, if you leave indexes there, subsequent calls will be even faster, and a simple addon update will let Odoo find and remove them.

openupgradelib/openupgrade.py
@@ -906,22 +906,49 @@ def update_workflow_workitems(cr, pool, ref_spec_actions):
             )
 
 
-def delete_model_workflow(cr, model):
+def delete_model_workflow(cr, model, drop_indexes=False):
     """
     Forcefully remove active workflows for obsolete models,
     to prevent foreign key issues when the orm deletes the model.
+
+    :param cr:
+        DB cursor.
+
+    :param str model:
+        Model name.
+
+    :param bool drop_indexes:
+        Do I drop indexes after finishing? If ``False``, those will be dropped
+        by a subsequent update of the ``workflow`` module in normal Odoo
+        probably.
     """
     # Save hours by adding needed indexes for affected FK constraints
-    index_name = sql.Identifier(get_legacy_name(
-        "wkf_triggers_workitem_id_index",
-    ))
-    logged_query(
-        cr,
-        sql.SQL("""
-            CREATE INDEX IF NOT EXISTS {} ON wkf_triggers
-            USING BTREE(workitem_id)
-        """).format(index_name)
-    )
+    to_index = {
+        "wkf_activity": ["subflow_id"],
+        "wkf_instance": ["wkf_id"],
+        "wkf_triggers": ["instance_id", "workitem_id"],
+        "wkf_workitem": ["act_id"],
+    }
+
+    def _index_loop():
+        for table, fields in to_index.items():
+            for col in fields:
+                index = sql.Identifier(
+                    "{tbl}_{col}_index".format(tbl=table, col=col),
+                )
+                table = sql.Identifier(table)
+                col = sql.Identifier(col)
+                yield index, table, col
+
+    for index, table, col in _index_loop():
+        logged_query(
+            cr,
+            sql.SQL("""
+                CREATE INDEX IF NOT EXISTS {} ON {}
+                USING BTREE({})
+            """).format(index, table, col)
+        )
+    # Delete workflows
     logged_query(
         cr,
         "DELETE FROM wkf_workitem WHERE act_id in "
@@ -933,8 +960,10 @@ def delete_model_workflow(cr, model):
     logged_query(
         cr,
         "DELETE FROM wkf WHERE osv = %s", (model,))
-    # Remove temporary index
-    logged_query(cr, sql.SQL("DROP INDEX {}").format(index_name))
+    # Remove temporary indexes if asked to do so
+    if drop_indexes:
+        for index, table, col in _index_loop():
+            logged_query(cr, sql.SQL("DROP INDEX {}").format(index))
 
 
 def warn_possible_dataloss(cr, pool, old_module, fields):

commit c567eed6541b2ed9f483d8d552fcc1ce90e028ff

[FIX] delete_model_workflow: Do not reassign variables
It seems like reassigning variables here changes the contents of the `to_index` dict, resulting in weird errors. Regression introduced in #189.

Fixed by changing variable names to avoid reusing them.

<details><summary>Failure logs</summary>

```
2019-10-31 14:33:16,150 1 ERROR prod OpenUpgrade: procurement: error in migration script procurement/migrations/8.0.1.0/pre-migration.py: TypeError('SQL identifiers must be strings',)
2019-10-31 14:33:16,150 1 ERROR prod OpenUpgrade: SQL identifiers must be strings
Traceback (most recent call last):
  File "/usr/local/lib/python2.7/dist-packages/openupgradelib/openupgrade.py", line 1691, in wrapped_function
    if use_env2 else cr, version)
  File "/opt/odoo/auto/addons/procurement/migrations/8.0.1.0/pre-migration.py", line 53, in migrate
    openupgrade.delete_model_workflow(cr, 'procurement.order')
  File "/usr/local/lib/python2.7/dist-packages/openupgradelib/openupgrade.py", line 944, in delete_model_workflow
    for index, table, col in _index_loop():
  File "/usr/local/lib/python2.7/dist-packages/openupgradelib/openupgrade.py", line 940, in _index_loop
    table = sql.Identifier(table)
  File "/usr/local/lib/python2.7/dist-packages/psycopg2/sql.py", line 311, in __init__
    raise TypeError("SQL identifiers must be strings")
TypeError: SQL identifiers must be strings
2019-10-31 14:33:16,152 1 CRITICAL prod openerp.service.server: Failed to initialize database `prod`.
Traceback (most recent call last):
  File "/opt/odoo/custom/src/odoo/openerp/service/server.py", line 941, in preload_registries
    registry = RegistryManager.new(dbname, update_module=update_module)
  File "/opt/odoo/custom/src/odoo/openerp/modules/registry.py", line 370, in new
    openerp.modules.load_modules(registry._db, force_demo, status, update_module)
  File "/opt/odoo/custom/src/odoo/openerp/modules/loading.py", line 420, in load_modules

openupgradelib/openupgrade.py
@@ -932,13 +932,13 @@ def delete_model_workflow(cr, model, drop_indexes=False):
     }
 
     def _index_loop():
-        for table, fields in to_index.items():
-            for col in fields:
+        for table_name, fields in to_index.items():
+            for col_name in fields:
                 index = sql.Identifier(
-                    "{tbl}_{col}_index".format(tbl=table, col=col),
+                    "{}_{}_index".format(table_name, col_name),
                 )
-                table = sql.Identifier(table)
-                col = sql.Identifier(col)
+                table = sql.Identifier(table_name)
+                col = sql.Identifier(col_name)
                 yield index, table, col
 
     for index, table, col in _index_loop():