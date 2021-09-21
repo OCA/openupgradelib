77513de21fb87ed367cb26b4bbe45faa78da5bba
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-09-21 16:12:35 +0200

    Publish to PyPI on git tag

--- setup.py
+++ setup.py
@@ -4,10 +4,7 @@
 import os
 import openupgradelib
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 
 with open('README.rst') as readme_file:
@@ -27,16 +24,21 @@ test_requirements = [
 
 setup(
     name='openupgradelib',
-    version=openupgradelib.__version__,
+    use_scm_version=True,
     description=openupgradelib.__doc__,
     long_description=readme + '\n\n' + history,
     author=openupgradelib.__author__,
     author_email=openupgradelib.__email__,
     url='https://github.com/OCA/openupgradelib',
     packages=['openupgradelib'],
-    package_dir={'openupgradelib': 'openupgradelib'},
     include_package_data=True,
-    install_requires=["lxml", "cssselect"],
+    setup_requires=["setuptools_scm"],
+    install_requires=[
+        "lxml",
+        "cssselect",
+        'importlib_metadata; python_version<"3.8"',
+    ],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     license=openupgradelib.__license__,
     zip_safe=False,
     keywords='openupgradelib',
@@ -46,11 +48,13 @@ setup(
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     test_suite='tests',
     tests_require=test_requirements
