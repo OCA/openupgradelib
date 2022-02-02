b540996ee8293c5dc22ffea202d22272b16f9a93
Miquel Raïch <miquel.raich@forgeflow.com>
2022-02-02 12:08:40 +0100

    [FIX] rename_models: also rename models in res_id of ir.property

--- openupgradelib/openupgrade.py
+++ openupgradelib/openupgrade.py
@@ -825,6 +825,16 @@ def rename_models(cr, model_spec):
             "UPDATE ir_filters SET model_id = %s "
             "WHERE model_id = %s", (new, old,),
         )
+        logged_query(
+            cr, """
+            UPDATE ir_property
+            SET res_id = replace(res_id, %(old_string)s, %(new_string)s)
+            WHERE res_id like %(old_pattern)s""", {
+                "old_pattern": "%s,%%" % old,
+                "old_string": "%s," % old,
+                "new_string": "%s," % new,
+            },
+        )
         # Handle properties that reference to this model
         logged_query(
             cr,
